fcaebaeeff996a7458fbf629a9f38e9dc41e7852
David Baque <baquedavid0@gmail.com>
2023-10-12 13:33:41 +0200

    add routes direcotory1

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,16 @@
-import express from 'express';
-import morgan from 'morgan';
+import express from "express";
+import morgan from "morgan";
+
+import apiRoutes from "@routes/index.routes";
 
 const app = express();
 
-app.use(morgan("dev"))
+app.use(morgan("dev"));
 
 app.get("/", (_, res) => {
-    res.json({message: "Hello"})
-})
+  res.json({ message: "Hello" });
+});
 
+app.use("/api", apiRoutes);
 
 export default app;
